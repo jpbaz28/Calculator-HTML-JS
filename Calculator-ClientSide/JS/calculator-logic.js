1c522658e3c7d1bf5ab5e928b4b7ca36fd7b6c30
jpbaz28 <jpbaz28@gmail.com>
2021-12-29 22:43:19 -0700

    Added a bit of style with some CSS

--- Calculator-ClientSide/JS/calculator-logic.js
+++ Calculator-ClientSide/JS/calculator-logic.js
@@ -2,16 +2,20 @@ let resultItems = [];
 const results = [];
 
 function calculate(event) {
-    const resultList = document.getElementById("result-list");
-    
+    //get element ids for our HTML elements
+    const resultList = document.getElementById("result-list"); 
     let value1st = document.getElementById("input1").value;
     let value2nd = document.getElementById("input2").value;
     const selection = document.getElementById("operator-select");
     let result = document.getElementById("result");
+    
+    //Determines selection of operator from HTML select element
     const operator = selection.options[selection.selectedIndex].value;
+    //turn sstrings into ints
     let num1 = parseInt(value1st);
     let num2 = parseInt(value2nd);
     console.log(operator, num1, num2);
+    //perform different operations depending on selection
     switch(operator) {
         case '+' :
             const sum = num1 + num2;
@@ -34,10 +38,12 @@ function calculate(event) {
             results.push(result.innerHTML);
             break;
         default:
-            result.innerHTML = "0";
+            result.innerHTML = "";
     }   
     console.log(results);
+    //map the results array into a list item adds a new item
     resultItems = results.map(r => `<li>${r}</li>`).join('');
+    //display the results in the list
     resultList.innerHTML = resultItems;
     num1 = 0;
     num2 = 0;
